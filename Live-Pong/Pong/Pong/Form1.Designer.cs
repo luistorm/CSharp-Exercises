cd30686fb6f8275cbe8a75eeeac9fc4f09468fe4
luis torres manjarres <luis torres manjarres>
2016-11-04 23:31:03 -0400

    Conexion establecida, aun no suma puntos pero se mueve

---
+++ Live-Pong/Pong/Pong/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Pong
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Location = new System.Drawing.Point(50, 31);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(10, 71);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Indigo;
+            this.panel2.Location = new System.Drawing.Point(831, 100);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(10, 71);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Pong.Properties.Resources.Ball;
+            this.pictureBox1.Location = new System.Drawing.Point(408, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(20, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 158);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(13, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(866, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "0";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(937, 79);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Comenzar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.button1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(927, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(64, 17);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Servidor";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(927, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(57, 17);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Cliente";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1048, 188);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ Live-Pong/Pong/Pong/Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Pong
+{
+    public partial class Form1 : Form
+    {
+        private int xB;
+        private int yB;
+        private int dB;
+        private Thread MoveBall;
+        private UdpClient udpServer;
+        private string dirGrupo;
+        private IPAddress multicastAddress;
+        private Thread tListen;
+        private IPEndPoint EPServer;
+        private UdpClient udpClient;
+        private IPEndPoint EPClient;
+        public Form1()
+        {
+            InitializeComponent();
+            xB = pictureBox1.Location.X;
+            yB = pictureBox1.Location.Y;
+            dB = 6;
+            CheckForIllegalCrossThreadCalls = false;
+            MoveBall = new Thread(Mover);
+            tListen = new Thread(Listen);
+            dirGrupo = "230.0.0.0";
+            EPServer = new IPEndPoint(IPAddress.Broadcast, 1234);
+            EPClient = new IPEndPoint(IPAddress.Any, 1234);
+            multicastAddress = IPAddress.Parse(dirGrupo);
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void Mover()
+        {
+            while (true)
+            {
+                if (dB == 3)//Bola derecha
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y);
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) > (panel2.Location.Y + 5 + panel2.Height / 2)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height))
+                        && (pictureBox1.Location.X + pictureBox1.Width) >= panel2.Location.X)
+                        dB = 7;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) >= (panel2.Location.Y)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height - 5))
+                        && (pictureBox1.Location.X + pictureBox1.Width) >= panel2.Location.X)
+                        dB = 8;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height / 2 + 5)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) > (panel2.Location.Y + panel2.Height / 2 - 5))
+                        && (pictureBox1.Location.X + pictureBox1.Width) >= panel2.Location.X)
+                        dB = 4;
+                }
+                if (dB == 4)
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y);
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) < (panel1.Location.Y + 5 + panel1.Height / 2)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) >= (panel1.Location.Y))
+                        && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 5;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) <= (panel1.Location.Y + panel1.Height)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) > (panel1.Location.Y + panel1.Height / 2 + 5))
+                        && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 6;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) >= (panel1.Location.Y + panel1.Height / 2 - 5)
+                       && (pictureBox1.Location.Y + pictureBox1.Height) < (panel1.Location.Y + panel1.Height / 2 + 5))
+                       && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 3;
+                }
+                if (dB == 5)//Bola diagonal superior derecha
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y - 5);
+                    if ((pictureBox1.Location.Y) <= 0)
+                        dB = 6;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) > (panel2.Location.Y + 5 + panel2.Height / 2) 
+                        && (pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height))
+                        && (pictureBox1.Location.X+pictureBox1.Width) >= panel2.Location.X)
+                        dB = 7;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) >= (panel2.Location.Y)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height-5))
+                        && (pictureBox1.Location.X + pictureBox1.Width) >= panel2.Location.X)
+                        dB = 8;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height / 2 + 5)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) > (panel2.Location.Y + panel2.Height/2 - 5))
+                        && (pictureBox1.Location.X + pictureBox1.Width) >= panel2.Location.X)
+                        dB = 4;
+                }
+                if (dB == 6)//Bola diagonal inferior derecha
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X + 5, pictureBox1.Location.Y + 5);
+                    if ((pictureBox1.Location.Y + pictureBox1.Height) >= 200)
+                        dB = 5;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) > (panel2.Location.Y + 5 + panel2.Height / 2)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height))
+                        && (pictureBox1.Location.X + pictureBox1.Width) >= panel2.Location.X)
+                        dB = 7;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) <= (panel2.Location.Y + panel2.Height / 2 + 5)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) > (panel2.Location.Y + panel2.Height / 2 - 5))
+                        && (pictureBox1.Location.X + pictureBox1.Width) >= panel2.Location.X)
+                        dB = 4;
+                }
+                if (dB == 7)//Bola diagonal inferior izquierda
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y + 5);
+                    if ((pictureBox1.Location.Y + pictureBox1.Height) >= 200)
+                        dB = 8;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) >= (panel1.Location.Y + panel1.Height / 2 - 5)
+                       && (pictureBox1.Location.Y + pictureBox1.Height) < (panel1.Location.Y + panel1.Height / 2 + 5))
+                       && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 3;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) <= (panel1.Location.Y + panel1.Height)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) > (panel1.Location.Y + panel1.Height / 2 + 5))
+                        && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 6;
+                }
+                if (dB == 8)//Bola diagonal superior izquierda
+                {
+                    pictureBox1.Location = new Point(pictureBox1.Location.X - 5, pictureBox1.Location.Y - 5);
+                    if ((pictureBox1.Location.Y + pictureBox1.Height) <= 0)
+                        dB = 7;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) > (panel1.Location.Y + 5 + panel1.Height / 2)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) <= (panel1.Location.Y + panel1.Height))
+                        && (pictureBox1.Location.X) >= (panel2.Location.X + panel2.Width))
+                        dB = 7;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) < (panel1.Location.Y + 5 + panel1.Height / 2)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) >= (panel1.Location.Y))
+                        && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 5;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) <= (panel1.Location.Y+panel1.Height)
+                        && (pictureBox1.Location.Y + pictureBox1.Height) > (panel1.Location.Y + panel1.Height/2 + 5))
+                        && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 6;
+                    if (((pictureBox1.Location.Y + pictureBox1.Height) >= (panel1.Location.Y + panel1.Height/2 -5)
+                       && (pictureBox1.Location.Y + pictureBox1.Height) < (panel1.Location.Y + panel1.Height / 2 + 5))
+                       && ((pictureBox1.Location.X) <= (panel1.Location.X + panel1.Width) && (pictureBox1.Location.X) >= (panel1.Location.X)))
+                        dB = 3;
+                }
+                Thread.Sleep(50);
+                if (pictureBox1.Location.X + pictureBox1.Width > panel2.Location.X + panel2.Width)
+                {
+                    pictureBox1.Location = new Point(xB, yB);
+                    dB = 6;
+                    label1.Text = (Convert.ToInt32(label1.Text) + 1).ToString();
+                }
+                if (pictureBox1.Location.X  < panel1.Location.X-10)
+                {
+                    pictureBox1.Location = new Point(xB, yB);
+                    dB = 6;
+                    label2.Text = (Convert.ToInt32(label2.Text) + 1).ToString();
+                }
+                string s = "*" + pictureBox1.Location.X.ToString() + "," + pictureBox1.Location.Y.ToString();
+                byte[] buff = Encoding.ASCII.GetBytes(s);
+                udpServer.Send(buff,buff.Length,EPServer);
+            }
+            
+        }
+
+        private void Listen()
+        {
+            while(Convert.ToInt32(label1.Text)<10 && Convert.ToInt32(label1.Text) < 10)
+            {
+                byte[] buff = new byte[1024];
+                buff = udpClient.Receive(ref EPClient);
+                if (buff == null)
+                    continue;
+                string s = Encoding.ASCII.GetString(buff);
+                if (s.ElementAt(0) == '*')
+                {
+                    s = s.Substring(1, s.Length - 1);
+                    string[] split = s.Split(',');
+                    int x, y;
+                    x = Convert.ToInt32(split[0]);
+                    y = Convert.ToInt32(split[1]);
+                    pictureBox1.Location = new Point(x, y);
+                }
+                if (s.ElementAt(0) == 'p')
+                {
+                    s = s.Substring(1, s.Length - 1);
+                    string[] split = s.Split(',');
+                    int x, y;
+                    x = Convert.ToInt32(split[0]);
+                    y = Convert.ToInt32(split[1]);
+                    panel1.Location = new Point(x, y);
+                }
+                if (s.ElementAt(0) == 'P')
+                {
+                    s = s.Substring(1, s.Length - 1);
+                    string[] split = s.Split(',');
+                    int x, y;
+                    x = Convert.ToInt32(split[0]);
+                    y = Convert.ToInt32(split[1]);
+                    panel2.Location = new Point(x, y);
+                }
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue =='W')
+            {
+                if (panel1.Location.Y - 5 >= 0)
+                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y - 5);
+                string s = "p" + panel1.Location.X.ToString() + "," + panel1.Location.Y.ToString();
+                byte[] buff = Encoding.ASCII.GetBytes(s);
+                udpServer.Send(buff, buff.Length, EPServer);
+            }
+            if (e.KeyValue == 'S')
+            {
+                if (((panel1.Location.Y + 5) + panel1.Height) <= this.Height)
+                    panel1.Location = new Point(panel1.Location.X, panel1.Location.Y + 5);
+                string s = "p" + panel1.Location.X.ToString() + "," + panel1.Location.Y.ToString();
+                byte[] buff = Encoding.ASCII.GetBytes(s);
+                udpServer.Send(buff, buff.Length, EPServer);
+            }
+            if (e.KeyValue=='O')
+            {
+                if (panel2.Location.Y - 5 >= 0)
+                    panel2.Location = new Point(panel2.Location.X, panel2.Location.Y - 5);
+                string s = "P" + panel2.Location.X.ToString() + "," + panel2.Location.Y.ToString();
+                byte[] buff = Encoding.ASCII.GetBytes(s);
+                udpServer.Send(buff, buff.Length, EPServer);
+            }
+            if (e.KeyValue=='L')
+            {
+                if (((panel2.Location.Y + 5) + panel2.Height) <= this.Height)
+                    panel2.Location = new Point(panel2.Location.X, panel2.Location.Y + 5);
+                string s = "P" + panel2.Location.X.ToString() + "," + panel2.Location.Y.ToString();
+                byte[] buff = Encoding.ASCII.GetBytes(s);
+                udpServer.Send(buff, buff.Length, EPServer);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            MoveBall.Abort();
+            tListen.Abort();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioButton1.Checked)
+            {
+                udpServer = new UdpClient();
+                udpServer.JoinMulticastGroup(multicastAddress);
+                MoveBall.Start();
+            }
+            else
+            {
+                udpClient = new UdpClient();
+                udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+                udpClient.Client.Bind(EPClient);
+                udpClient.JoinMulticastGroup(multicastAddress);
+                tListen.Start();
+            }
+        }
+    }
+}
